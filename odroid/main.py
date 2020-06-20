a163af5b8735baa42b9095773cb8cb654aa0668d
Alexander Gruzintsev <overtired@yandex-team.ru>
2020-06-20 22:00:40 +0300

    Sensors info could be fetched

--- odroid/main.py
+++ odroid/main.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python
+
 from bme280 import readBME280All
 from datetime import datetime
 from mh_z19 import get_co2
 
 import json
+import requests
+import signal
+import sys
+import syslog
 import time
+
+interrupted = False
+
+def sigint_handler(sig, frame):
+    global interrupted
+    print("Fetching sensors daemon is stopping...", file=sys.stderr)
+    interrupted = True
+    syslog.syslog(syslog.LOG_INFO, "Fetching sensors info was stopped")
 
 
 def get_values():
@@ -19,4 +33,16 @@ def get_values():
     }
 
 if __name__ == "__main__":
-    print(json.dumps(get_values()))
+    signal.signal(signal.SIGINT, sigint_handler)
+    sleep_time_s = 1
+    while not interrupted:
+        data = json.dumps(get_values())
+        response = requests.post(url = "http://192.168.1.66:443/sensors", data = data)
+        print(
+            "Sent sensors data, http code = {}; body = {}".format(
+                response.status_code,
+                response.text
+            ),
+            file=sys.stderr
+        )
+        time.sleep(sleep_time_s)
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,11 +2,36 @@
 
 #[macro_use] extern crate rocket;
 
+static mut received_updates: std::vec::Vec::<std::string::String> = std::vec::Vec::<std::string::String>::new();
+static mut received_sensors: std::vec::Vec::<std::string::String> = std::vec::Vec::<std::string::String>::new();
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    let updates = unsafe {
+        received_updates.join("\n")
+    };
+    let sensors = unsafe {
+        received_sensors.join("\n")
+    };
+    return format!("Updates:\n{}\nSensors:\n{}", updates, sensors);
+}
+
+#[post("/sensors", data = "<data>")]
+fn sensors(data: String) ->&'static str {
+    unsafe {
+        received_sensors.push(data);
+    }
+    return "Ok";
+}
+
+#[post("/updates", data = "<data>")]
+fn updates(data: String) ->&'static str {
+    unsafe {
+        received_updates.push(data);
+    }
+    return "Ok";
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, sensors, updates]).launch();
 }
